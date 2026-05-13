8edfafac5fef652cff2ff27e8337692fbc36a589
Aleksandra Uspenskaia <aleksandra.uspenskaia@sumup.com>
2026-05-13 18:09:04 +0200

    Add API interface and data model for country information

---
+++ app/src/main/java/com/sumup/countryapp/api/CountryAppApi.kt
@@ -0,0 +1,39 @@
+package com.sumup.countryapp.api
+
+import com.sumup.countryapp.datamodels.CountryResponse
+import okhttp3.Response
+import retrofit2.http.GET
+import retrofit2.http.Path
+import retrofit2.http.Query
+import retrofit2.http.QueryMap
+import javax.inject.Inject
+
+interface CountryAppApi{
+
+    @GET("all")
+    suspend fun getCountries(@Query("fields") fields:String): retrofit2.Response<List<CountryResponse>>
+
+    @GET("name/{name}")
+    suspend fun getCountryByName(@Path("name") countryName:String): retrofit2.Response<CountryResponse>
+
+    @GET("region/{region}")
+    suspend fun getCountriesByRegion(@Path("region") regionName:String): retrofit2.Response<List<CountryResponse>>
+
+    @GET("subregion/{subregion}")
+    suspend fun getCountriesBySubRegion(@Path("subregion") subRegionName:String): retrofit2.Response<List<CountryResponse>>
+
+    @GET("capital/{capital}")
+    suspend fun getCountriesByCapital(@Path("capital") capitalName:String): retrofit2.Response<CountryResponse>
+
+    @GET("currency/{currency}")
+    suspend fun getCountriesByCurrency(@Path("currency") currencyName:String): retrofit2.Response<List<CountryResponse>>
+
+    @GET("lang/{language}")
+    suspend fun getCountriesByLanguage(@Path("language") languageName:String): retrofit2.Response<List<CountryResponse>>
+
+    @GET("alpha/{code}")
+    suspend fun getCountryByCode(@Path("code") countryCode:String): retrofit2.Response<CountryResponse>
+
+    @GET("independent")
+    suspend fun getCountriesByIndependence(@Query("status") independenceStatus:Boolean): retrofit2.Response<List<CountryResponse>>
+}
